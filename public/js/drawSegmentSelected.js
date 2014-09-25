624e8dd04e4b44a2795bf6804591b120c1cdfd32
Tunglk <luongkhactung@gmail.com>
2014-09-25 09:10:59 +0700

    Signed-off-by: Tunglk <luongkhactung@gmail.com>

--- public/js/drawSegmentSelected.js
+++ public/js/drawSegmentSelected.js
@@ -8,8 +8,11 @@ function choiceSegment(event){
     var disx = segments[i][0]+segments[i][2]*event.scale;
     var disy = segments[i][1]+segments[i][3]*event.scale
     if(event.xImageTouch >= segments[i][0] && event.xImageTouch <= disx && event.yImageTouch >= segments[i][1] && event.yImageTouch <= disy){
-      console.log("Segment: ",i);
-      $("#segment-selected").css({width:segments[i][2]*event.scale+'px', height:segments[i][3]*event.scale+'px', top:segments[i][1]-event.yOImage+'px', left:segments[i][0]-event.xOImage+"px", border: "3px solid red"});
+      var xDraw = Math.floor((parseFloat(segments[i][0])*parseFloat(event.scale))+parseFloat(event.xOImage));
+      var yDraw = Math.floor((parseFloat(segments[i][1])*parseFloat(event.scale))+parseFloat(event.yOImage));
+      alert(xDraw);
+      alert(yDraw);
+      $("#segment-selected").css({width:segments[i][2]*event.scale+'px', height:segments[i][3]*event.scale+'px', top:yDraw.toString()+'px', left:xDraw.toString()+"px", border: "3px solid red"});
     }
     else{
     }
