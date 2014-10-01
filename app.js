8a6c7b16bca0a2cc8926020b1a67da5dbb6b3831
Tunglk <luongkhactung@gmail.com>
2014-10-01 15:23:25 +0700

    tunglk

--- app.js
+++ app.js
@@ -2,17 +2,23 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-
 var app = express();
 
 // -------------- environments --------------
-app.use(express.cookieParser('very secret'));
-app.use(express.session());
+//app.use(express.cookieParser('very secret'));
+//app.use(express.session());
 app.set('port', process.env.PORT || 3200);
-app.use(app.router);
+app.use(express.methodOverride());
+app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.post('/touchmove', function( req, res ){
+  console.log("/touchmove",req.body);
+  res.send({tung:"tung"});
+});
+app.post('/touchimage', function( req, res ){
+  console.log("/touchimage",req.body);
+  res.send({tung:"tung"});
+});
 //----------------- start server -------------------------
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
